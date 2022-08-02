79a4586f5d24986325de19e090c720f2fbad2ba9
lena <srgv.lena@gmail.com>
2022-08-02 13:12:55 +0300

    sum kernel

--- src/cl/func_recv_2D_sum.cl
+++ src/cl/func_recv_2D_sum.cl
@@ -3,26 +3,70 @@
 //#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
 
 
-__kernel void sum(__global const int* input, int n,
-                  volatile __global int* res){
+__kernel void sum(__global const float* prod_vals, 
+                  int K_x, int K_y, 
+                  int N, int r,
+                  volatile __global float* result){
 
-    int localID = get_local_id(0);
-    int globalID = get_global_id(0);
+    int localID_x = get_local_id(0);
+    int globalID_x = get_global_id(0);
 
-    __local int input_local[WORK_GROUP_SIZE];
-    input_local[localID] = input[globalID];
+    int localID_y = get_local_id(1);
+    int globalID_y = get_global_id(1);
 
-    barrier(CLK_LOCAL_MEM_FENCE);
+    int Nr = N*r/4;
 
-    for (int n_val = WORK_GROUP_SIZE; n_val>1; n_val/=2){
-        if(2*localID < n_val){
-            input_local[localID]+=input_local[localID+n_val/2];
-        }
-        barrier(CLK_LOCAL_MEM_FENCE);
+    //индекс ближайшей точки крупной сетки слева-сверху
+    //т.е. округление вниз!!
+    int k_x = globalID_x / N;
+    int k_y = globalID_y / N;
+
+    //индекс в ячейке мелкой сетки 
+    int m_x = globalID_x % N;
+    int m_y = globalID_y % N;
+
+    //индекс в мелкой сетке
+    int x = globalID_x;
+    int y = globalID_y;
+
+    //__local int input_local[WORK_GROUP_SIZE];
+    //input_local[localID] = input[globalID];
+
+    //barrier(CLK_LOCAL_MEM_FENCE); //барьер по чтению из VRAM
+
+    //инициализация суммы в точке мелной сетки
+    result[y*(N*K_x) + x] = prod_vals[(k_x+k_y)*Nr*Nr + m_y*Nr + m_x];
+
+    //суммирование по оси х
+    // "убывающая" половина, исключая начальную точку
+    for(int i = 1; i < r/2; i++){
+        if((k_x - i)>=0)
+            result[y*(N*K_x) + x] += prod_vals[((k_x-i)+k_y*K_x)*Nr*Nr + m_y*Nr + (i*N + m_x)];
+    }
+    // "возрастающая" половина
+    for(int i = 0; i < r/2; i++){
+        if((k_x + i)<=K_x)
+            result[y*(N*K_x) + x] += prod_vals[((k_x+i)+k_y*K_x)*Nr*Nr + m_y*Nr + (i*N - m_x)];
     }
     
+
+    //суммирование по оси y  --- аналогично
+    // "убывающая" половина, исключая начальную точку
+    for(int i = 1; i < r/2; i++){
+        if((k_y - i)>=0)
+            result[y*(N*K_x) + x] += prod_vals[(k_x+(k_y-i)*K_x)*Nr*Nr + (i*N + m_y)*Nr + m_x];
+    }
+    // "возрастающая" половина
+    for(int i = 0; i < r/2; i++){
+        if((k_y + i)<=K_y)
+            result[y*(N*K_x) + x] += prod_vals[(k_x+(k_y+i)*K_x)*Nr*Nr + (i*N - m_y)*Nr + m_x];
+    }
+
+    /*
+    //atomics example
     if(localID==0){
         atom_add(res, input_local[localID]);
     }
+    */
     
 }
--- src/main_func_recv_2D.cpp
+++ src/main_func_recv_2D.cpp
@@ -6,8 +6,12 @@
 #include <libgpu/shared_device_buffer.h>
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:22
-#include "cl/sum_cl.h"
+//#include "cl/sum_cl.h"
+#include "cl/func_recv_2D_prod_cl.h"
+#include "cl/func_recv_2D_sum_cl.h"
 
+
+#include <vector>
 
 
 template<typename T>
@@ -26,45 +30,58 @@ int main(int argc, char **argv)
 {
     int benchmarkingIters = 10;
 
-    unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
-    std::vector<unsigned int> as(n, 0);
-    FastRandom r(42);
-    for (int i = 0; i < n; ++i) {
-        as[i] = (unsigned int) r.next(0, std::numeric_limits<unsigned int>::max() / n);
-        reference_sum += as[i];
-    }
+// Определяем размер задачи 
 
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
-            for (int i = 0; i < n; ++i) {
-                sum += as[i];
-            }
-            EXPECT_THE_SAME(reference_sum, sum, "CPU result should be consistent!");
-            t.nextLap();
-        }
-        std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
-    }
+// Кол-во узлов крупной сетки
+int K_x = 128;
+int K_y = 128; // а если K_x!=K_y ??
+int K = K_x*K_y;
 
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
-            #pragma omp parallel for reduction(+:sum)
-            for (int i = 0; i < n; ++i) {
-                sum += as[i];
-            }
-            EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
-            t.nextLap();
-        }
-        std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
-    }
+//Количество узлов мелкой сетки в интервале крупной
+//без учёта левой границы
+//одинаковое по каждой координате
+int N = 5; //==N_x ==N_y ??
 
-    {
+//Количество узлов мелкой сетки
+int M_x = (N+1)*K_x;
+int M_y = (N+1)*K_y;
+int M = M_x*M_y;
+
+//Порядок ядра (чётное значение)
+int r = 2; //4
+
+// Размер !четверти! ядра
+int Nr = (N+1)*r/4; 
+
+// Массивы (матрицы) в DRAM
+// (host memory)
+
+std::vector<float> in_data(K); //NB: в общем случае значения мб не только float, но и вектора
+std::vector<float> out_data(M);
+std::vector<float> out_data_etal(M);
+
+// значения ядра 
+//храним четверть (правую нижнюю) с учётом симметричности по обоим координатам
+std::vector<float> kern_vals(Nr*Nr); 
+
+// значения произведений
+// для каждого элемента крупной сетки хранится вычисляется матрица произведений
+std::vector<float> prod_vals((Nr*Nr)*K); //(((N+1)*r/2)*((N+1)*r/2)*K); //результат работы kern_prod
+
+
+// Имитируем входные данные :: крупную сетку 
+// TODO::использовать imit_test_fun_2d
+{
+
+}
+
+// Считаем эталон 
+{
+
+}
+
+//
+{
     // chooseGPUDevice:
     // - Если не доступо ни одного устройства - кинет ошибку
     // - Если доступно ровно одно устройство - вернет это устройство
@@ -80,43 +97,80 @@ int main(int argc, char **argv)
         context.activate();
 
     // Создаем буфер в видеопамяти
-        gpu::gpu_mem_32u as_gpu;
-        as_gpu.resizeN(n);
+        gpu::gpu_mem_32f in_data_gpu;
+        in_data_gpu.resizeN(K);
 
-        gpu::gpu_mem_32u res_gpu;
-        res_gpu.resizeN(1);
+        gpu::gpu_mem_32f out_data_gpu;
+        out_data_gpu.resizeN(M);
+
+        gpu::gpu_mem_32f kern_vals_gpu;
+        kern_vals_gpu.resizeN(Nr*Nr);
+
+        gpu::gpu_mem_32f prod_vals_gpu;
+        prod_vals_gpu.resizeN((Nr*Nr)*K);
     
     // Исходники кернела написаны в src/cl/aplusb.cl
     // Но благодаря convertIntoHeader(src/cl/aplusb.cl src/cl/aplusb_cl.h aplusb_kernel) (см. CMakeLists.txt:18)
     // при компиляции автоматически появится файл src/cl/aplusb_cl.h с массивом aplusb_kernel состоящим из байт исходника
     // т.о. программе не будет нужно в runtime читать файл с диска, т.к. исходник кернелов теперь хранится в массиве данных основной программы
-        ocl::Kernel kern_sum(sum_kernel, sum_kernel_length, "sum");
-        kern_sum.compile(false);
+        //ocl::Kernel kern_sum(sum_kernel, sum_kernel_length, "sum");
+        //kern_sum.compile(false);
 
-        unsigned int workGroupSize = 128;
-        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+    ocl::Kernel kern_prod(prod_kernel, prod_kernel_length, "prod");
+    kern_prod.compile(true);
 
-        timer t;  
-        for (int i=0; i<benchmarkingIters; ++i){
+    ocl::Kernel kern_sum(sum_kernel, sum_kernel_length, "sum");
+    kern_sum.compile(true);
+
+// 2D размер задачи для kern_prod
+        unsigned int workGroupSize_1 = 128;
+        unsigned int global_work_size_1 = (K + workGroupSize_1 - 1) / workGroupSize_1 * workGroupSize_1; //for kern_prod
+
+
+// 2D размер задачи для kern_sum
+        unsigned int workGroupSize_2x = 32;
+        unsigned int workGroupSize_2y = 32;
+        unsigned int global_work_size_2x = (M_x + workGroupSize_2x - 1) / workGroupSize_2x * workGroupSize_2x;
+        unsigned int global_work_size_2y = (M_y + workGroupSize_2y - 1) / workGroupSize_2y * workGroupSize_2y;
+//
+
+        //timer t;  
+        //for (int i=0; i<benchmarkingIters; ++i){
+
         // Прогружаем данные из векторов as
         // DRAM --> VRAM
         // (есть нетипизированный метод write для которого количество измеряется в байтах,
         // и типизированный writeN, для которого количество измеряется в количестве float-элементов, т.к. gpu::gpu_mem_32f - это shared_device_buffer_typed<float>)
-            as_gpu.writeN(as.data(), n);
-            unsigned int sum = 0;
-            res_gpu.writeN(&sum, 1);
+            in_data_gpu.writeN(in_data.data(), K);
+            out_data_gpu.writeN(out_data.data(), M);
+            kern_vals_gpu.writeN(kern_vals.data(), (Nr*Nr));
+            prod_vals_gpu.writeN(prod_vals.data(), (Nr*Nr)*K);
 
-            kern_sum.exec(gpu::WorkSize(workGroupSize, global_work_size),as_gpu, n, res_gpu);
+            kern_prod.exec( gpu::WorkSize(workGroupSize_1, global_work_size_1),
+                            in_data_gpu, K, 
+                            kern_vals_gpu, Nr, 
+                            prod_vals_gpu);
+            std::cout<<"[kern_prod] exec done"<<std::endl;
 
-            res_gpu.readN(&sum, 1);
+            kern_sum.exec( gpu::WorkSize(   workGroupSize_2x, workGroupSize_2y, 
+                                            global_work_size_2x, global_work_size_2y),
+                            prod_vals_gpu, 
+                            K_x, K_y, 
+                            (N+1), r, 
+                            out_data_gpu);
+        
+            std::cout<<"[kern_sum] exec done"<<std::endl;
 
-            EXPECT_THE_SAME(reference_sum, sum, "GPU <OCL kern> result should be consistent!");
+            //res_gpu.readN(&sum, 1); //VRAM -> DRAM
+
+            //EXPECT_THE_SAME(reference_sum, sum, "GPU <OCL kern> result should be consistent!");
 
             //std::cout<<"reference_sum = "<< reference_sum <<" :: sum = "<< sum <<std::endl;
-            t.nextLap();
-        }
-        std::cout << "GPU <OCL kern>: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU <OCL kern>: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+
+            //t.nextLap();
+        //}
+        //std::cout << "GPU <OCL kern>: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        //std::cout << "GPU <OCL kern>: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
 
     }
     return 0;
